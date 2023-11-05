98d1852ffdeb19de8ce2e142b2277663594c5d54
Huseyin Karatas <huseyin.karatas@metu.edu.tr>
2023-11-05 10:59:35 +0300

    Guide section

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,4 @@
-// NAVIGATION
-export const NAV_LINKS = [
+ export const NAV_LINKS = [
     { href: '/', key: 'home', label: 'Home' },
     { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
     { href: '/', key: 'services', label: 'Services' },
