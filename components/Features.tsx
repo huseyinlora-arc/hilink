ab0a6d10c883500b474a779b01100f913417df06
Huseyin Karatas <huseyin.karatas@metu.edu.tr>
2023-11-07 17:22:35 +0300

    Fixed the bug in the Features section

--- components/Features.tsx
+++ components/Features.tsx
@@ -2,6 +2,47 @@ import { FEATURES } from "@/constants";
 import Image from "next/image";
 import React from "react";
 
+const Features = () => {
+  return (
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full flex justify-end">
+        <div className="flex flex-1  flex-row justify-start">
+          <Image
+            src="/phone.png"
+            alt="phone"
+            width={428}
+            height={815}
+            className="feature-phone"
+          />
+        </div>
+
+        <div className="z-20 flex w-full flex-col lg:w-[60%]">
+          <div className="relative">
+            <Image
+              src="/camp.svg"
+              alt="camp"
+              width={50}
+              height={50}
+              className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
+            />
+            <h2 className="bold-40 lg:bold-64">Our Features</h2>
+          </div>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
+            {FEATURES.map((feature) => (
+              <FeatureItem
+                key={feature.title}
+                title={feature.title}
+                icon={feature.icon}
+                description={feature.description}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 type FeatureItem = {
   title: string;
   icon: string;
@@ -10,7 +51,7 @@ type FeatureItem = {
 
 const FeatureItem = ({ title, icon, description }: FeatureItem) => {
   return (
-    <li className="flex w-full flex-1 flex-col items-start ">
+    <li className="flex w-full flex-1 flex-col items-start">
       <div className="rounded-full p-4 lg:p-7 bg-green-50">
         <Image src={icon} alt="map" width={28} height={28} />
       </div>
@@ -19,46 +60,6 @@ const FeatureItem = ({ title, icon, description }: FeatureItem) => {
         {description}
       </p>
     </li>
-  );
-};
-
-const Features = () => {
-  return (
-    <section className=" flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
-      <div className="max-container flex padding-container relative w-full justify-end">
-        <div className="flex flex-1 lg:min-h-[900px]">
-          <Image
-            src="/phone.png"
-            alt="phone"
-            width={440}
-            height={1000}
-            className="feature-phone"
-          />
-        </div>
-        <div className="z-20 flex w-full flex-col lg:w-[60%]">
-          <div className="relative ">
-            <Image
-              src="/camp.svg"
-              alt="camp"
-              width={50}
-              height={50}
-              className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px] "
-            />
-            <h2 className="bold-40 lg:bold-60 ">Our Features</h2>
-          </div>
-        </div>
-        <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20">
-          {FEATURES.map((feature) => (
-            <FeatureItem
-              title={feature.title}
-              key={feature.title}
-              icon={feature.icon}
-              description={feature.description}
-            />
-          ))}
-        </ul>
-      </div>
-    </section>
   );
 };
 
